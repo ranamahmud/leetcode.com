24d7cd867006cfdfcf24d9fc08dda6a8a4660156
Md Rana Mahmud <ranamd0@gmail.com>
2020-02-16 02:42:49 +0600

    520 detect capital solved

---
+++ 520.detect-capital.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=520 lang=python3
+#
+# [520] Detect Capital
+#
+
+# @lc code=start
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper():
+            return True
+        elif word.islower():
+            return True
+        elif word.capitalize() == word:
+            return True
+        else:
+            return False
+        
+        
+# @lc code=end
+
---
+++ 724.find-pivot-index.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=724 lang=python3
+#
+# [724] Find Pivot Index
+#
+
+# @lc code=start
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        
+# @lc code=end
+
